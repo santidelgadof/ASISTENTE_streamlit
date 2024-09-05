789ac2c9afb00bf810c58318c5c0c75411ce469b
Santiago Delgado Ferreiro <santiago.delgadof@udc.es>
2024-09-05 12:35:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
-from sqlalchemy import create_engine
-import pymysql
+from sqlalchemy import create_engine, text
 import streamlit as st
+import pymysql
 
 # Configuración de la conexión a la base de datos
 def get_db_engine():
     engine = create_engine(
         f"mysql+pymysql://{st.secrets['DB_USER']}:{st.secrets['DB_PASSWORD']}@{st.secrets['DB_HOST']}:{st.secrets['DB_PORT']}/{st.secrets['DB_NAME']}",
-        connect_args={'charset': 'utf8mb4'},  # Mantén el charset si es necesario
+        connect_args={'charset': 'utf8mb4'},
         echo=False
     )
     return engine
+
+# Función para ejecutar una consulta
+def run_query(query):
+    engine = get_db_engine()
+    with engine.connect() as connection:
+        result = connection.execute(text(query))  # Usar `text()` para escribir consultas SQL
+        return result.fetchall()
